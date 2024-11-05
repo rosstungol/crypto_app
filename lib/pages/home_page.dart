932a16890ba6ccecff5f7848ef2273fd9a02ccf1
Ross Jonathan Tungol <rossjstungol@gmail.com>
2024-11-05 20:35:21 +0800

    Implements static get dialog

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+
+import '../widgets/add_asset_dialog.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: _appBar(context),
+    );
+  }
+
+  PreferredSizeWidget _appBar(BuildContext context) {
+    return AppBar(
+      title: const CircleAvatar(
+        backgroundImage: NetworkImage('https://i.pravatar.cc/150?img=3'),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {
+            Get.dialog(const AddAssetDialog());
+          },
+          icon: const Icon(Icons.add),
+        )
+      ],
+    );
   }
 }
---
+++ lib/widgets/add_asset_dialog.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AddAssetDialog extends StatelessWidget {
+  const AddAssetDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Material(
+        child: Container(
+          height: MediaQuery.sizeOf(context).height * 0.40,
+          width: MediaQuery.sizeOf(context).width * 0.80,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.white,
+          ),
+          child: _buildUI(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUI() {
+    return const Column(
+      children: [],
+    );
+  }
+}
